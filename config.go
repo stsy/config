7fda206aa9a9e7e56a16bc0d17e686810c022063
stsy <nem989@gmail.com>
2019-02-21 22:42:53 +0100

    Returns a map of json file, example works

--- config.go
+++ config.go
@@ -1,1 +1,22 @@
 package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+// Open config file return a map
+func Open(path string) (config map[string]string, err error) {
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		return
+	}
+
+	config = map[string]string{}
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		return
+	}
+
+	return
+}
--- examples/main.go
+++ examples/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/stsy/config"
+)
+
+func main() {
+	conf, err := config.Open("./config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Prints "world" form key "hello"
+	fmt.Println(conf["hello"])
 }
